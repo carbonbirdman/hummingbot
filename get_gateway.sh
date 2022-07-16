3d2fd87724e21e45ed1fe6a4c3d77fedb5eacdfe
carbonbirdman <carbonbirdman@gmail.com>
2022-07-16 12:31:48 +1000

    add bespoke gateway files

---
+++ get_gateway.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/development/installation/docker-commands/create-gateway.sh -o create-gateway.sh
+curl https://raw.githubusercontent.com/CoinAlpha/hummingbot/development/installation/docker-commands/update-gateway.sh -o update-gateway.sh
+chmod a+x *.sh
+./create-gateway.sh
